c7ecd590c18d8eaaed000a040716f5a0f8bbda53
Sudhir Boddu <bsudhir6@gmail.com>
2017-11-20 15:16:54 +0530

    List Exercise - 1

---
+++ Exercise6.py
@@ -0,0 +1,11 @@
+l = [1,2,3,4]
+res = []
+for e in l:
+    res.append(e * e)
+print(res)
+
+##Pseudo Code
+##create empty list
+##take each element(e) from l
+##    square it
+##    and append to empty list
