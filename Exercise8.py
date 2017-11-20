33974a30588fc6ffd3429eee4bd307bc6171dc61
Sudhir Boddu <bsudhir6@gmail.com>
2017-11-20 16:31:25 +0530

    Exercise 8 added

---
+++ Exercise8.py
@@ -0,0 +1,15 @@
+s = '[1,2,3,4,5]'
+res=[]
+s = s.strip("[").strip("]")
+s = s.split(",")
+for e in s:    
+    res.append(int(e))
+print(res)
+
+##s='[1,2,3,4,5]'
+##create empty list
+##strip '[', ']' charecters out of s
+##split s with ','
+##Take each element from above
+##    convert element to int
+##    and append to empty list
